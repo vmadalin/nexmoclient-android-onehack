de04670a71dec0696c4ada466fb27fb739661771
Madalin Valceleanu <devmobile@vmadalin.com>
2020-06-15 15:11:15 +0100

    Add a simple implementation about incoming-call

--- app/src/main/java/com/nexmo/onehack/features/calls/incoming/IncomingCallViewModel.kt
+++ app/src/main/java/com/nexmo/onehack/features/calls/incoming/IncomingCallViewModel.kt
@@ -1,7 +1,42 @@
 package com.nexmo.onehack.features.calls.incoming
 
+import android.Manifest
+import androidx.annotation.RequiresPermission
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nexmo.client.NexmoCall
+import com.nexmo.client.NexmoClient
+import com.nexmo.client.request_listener.NexmoApiError
+import com.nexmo.client.request_listener.NexmoRequestListener
 
-class IncomingCallViewModel : ViewModel() {
-    // TODO
+class IncomingCallViewModel(
+    private val nexmoClient: NexmoClient,
+    private val nexmoCall: NexmoCall?
+) : ViewModel() {
+
+    val callerName = nexmoCall?.callMembers?.first()?.user?.name ?: run { "UNKNOWN" }
+
+    private val _state = MutableLiveData<IncomingCallViewStates>()
+    val state: LiveData<IncomingCallViewStates>
+        get() = _state
+
+    private val answerCallListener = object : NexmoRequestListener<NexmoCall> {
+        override fun onSuccess(call: NexmoCall?) {
+            _state.postValue(IncomingCallViewStates.CALL_ACCEPTED)
+        }
+
+        override fun onError(apiError: NexmoApiError) {
+            _state.postValue(IncomingCallViewStates.CALL_ACCEPT_ERROR)
+        }
+    }
+
+    fun rejectCall() {
+        _state.postValue(IncomingCallViewStates.CALL_REJECTED)
+    }
+
+    @RequiresPermission(Manifest.permission.RECORD_AUDIO)
+    fun answerCall() {
+        nexmoCall?.answer(answerCallListener)
+    }
 }
--- app/src/main/java/com/nexmo/onehack/features/calls/incoming/IncomingCallViewModelFactory.kt
+++ app/src/main/java/com/nexmo/onehack/features/calls/incoming/IncomingCallViewModelFactory.kt
@@ -2,11 +2,15 @@ package com.nexmo.onehack.features.calls.incoming
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.nexmo.client.NexmoClient
+import com.nexmo.onehack.features.calls.CallManager
 
 class IncomingCallViewModelFactory : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T =
-        // TODO
-        null as T
+        IncomingCallViewModel(
+            nexmoCall = CallManager.onGoingCall,
+            nexmoClient = NexmoClient.get()
+        ) as T
 }
