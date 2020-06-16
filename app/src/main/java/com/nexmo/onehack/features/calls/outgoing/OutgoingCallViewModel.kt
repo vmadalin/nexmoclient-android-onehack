8a2f3c36627b6b8207d58bc4bf08f5aa7274f1e2
John Peters <john.peters@vonage.com>
2020-06-16 13:34:15 +0100

    merge with all branches

--- app/src/main/java/com/nexmo/onehack/features/calls/outgoing/OutgoingCallViewModel.kt
+++ app/src/main/java/com/nexmo/onehack/features/calls/outgoing/OutgoingCallViewModel.kt
@@ -1,18 +1,12 @@
 package com.nexmo.onehack.features.calls.outgoing
 
-import android.Manifest
-import androidx.annotation.RequiresPermission
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.nexmo.client.NexmoCall
-import com.nexmo.client.NexmoClient
-import com.nexmo.client.request_listener.NexmoApiError
-import com.nexmo.client.request_listener.NexmoRequestListener
-import com.nexmo.onehack.features.calls.outgoing.OutgoingCallViewStates
 
 class OutgoingCallViewModel(
-    private val nexmoClient: NexmoClient,
     private val nexmoCall: NexmoCall?
 ) : ViewModel() {
 
