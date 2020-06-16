82121b9de1cac1987fe6cedcd6ec924a7681c1c6
Madalin Valceleanu <devmobile@vmadalin.com>
2020-06-16 08:27:30 +0100

    Fast workarround to hide toolbar for splash

--- app/src/main/java/com/nexmo/onehack/features/splash/SplashFragment.kt
+++ app/src/main/java/com/nexmo/onehack/features/splash/SplashFragment.kt
@@ -3,6 +3,7 @@ package com.nexmo.onehack.features.splash
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.nexmo.onehack.R
@@ -14,5 +15,15 @@ class SplashFragment: Fragment(R.layout.fragment_splash) {
         Handler().postDelayed({
             findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToLoginFragment())
         }, 2000)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity).supportActionBar?.show()
     }
 }
